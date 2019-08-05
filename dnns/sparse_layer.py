731d758a82cbe07a2d5fcb6557b6ff35724133bb
Petrut Antoniu Bogdan <petrut.bogdan@manchester.ac.uk>
2019-08-05 13:39:32 +0100

    as in Keras, separated SparseConv2D and _SparseConv. Added initial skeleton (copy paste) DepthwiseConv2D

--- dnns/sparse_layer.py
+++ dnns/sparse_layer.py
@@ -131,7 +131,7 @@ class Sparse(Layer):
         return dict(list(base_config.items()) + list(config.items()))
 
 
-class SparseConv2D(Layer):
+class _SparseConv(Layer):
     """Abstract nD convolution layer (private, used as implementation base).
 
     This layer creates a convolution kernel that is convolved
@@ -189,11 +189,11 @@ class SparseConv2D(Layer):
     """
 
     def __init__(self,
+                 rank,
                  filters,
                  kernel_size,
                  connectivity_level=None,
                  connectivity_decay=None,
-                 rank=2,
                  strides=1,
                  padding='valid',
                  data_format=None,
@@ -208,7 +208,7 @@ class SparseConv2D(Layer):
                  kernel_constraint=None,
                  bias_constraint=None,
                  **kwargs):
-        super(SparseConv2D, self).__init__(**kwargs)
+        super(_SparseConv, self).__init__(**kwargs)
         self.rank = rank
         self.filters = filters
         self.kernel_size = conv_utils.normalize_tuple(kernel_size, rank,
@@ -281,10 +281,10 @@ class SparseConv2D(Layer):
                                     axes={channel_axis: input_dim})
 
         # Be sure to call this at the end
-        super(SparseConv2D, self).build(input_shape)
+        super(_SparseConv, self).build(input_shape)
 
     def add_update(self, updates, inputs=None):
-        super(Sparse, self).add_update(updates, inputs)
+        super(_SparseConv, self).add_update(updates, inputs)
 
     def get_number_of_active_connections(self):
         total_number_of_matrix_entries = np.prod(self.kernel_shape)
@@ -375,5 +375,312 @@ class SparseConv2D(Layer):
             'connectivity_level': self.connectivity_level,
             'connectivity_decay': self.connectivity_decay,
         }
-        base_config = super(SparseConv2D, self).get_config()
+        base_config = super(_SparseConv, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
+
+
+class SparseConv2D(_SparseConv):
+    """Abstract nD convolution layer (private, used as implementation base).
+
+    This layer creates a convolution kernel that is convolved
+    with the layer input to produce a tensor of outputs.
+    If `use_bias` is True, a bias vector is created and added to the outputs.
+    Finally, if `activation` is not `None`,
+    it is applied to the outputs as well.
+
+    # Arguments
+        rank: An integer, the rank of the convolution,
+            e.g. "2" for 2D convolution.
+        filters: Integer, the dimensionality of the output space
+            (i.e. the number of output filters in the convolution).
+        kernel_size: An integer or tuple/list of n integers, specifying the
+            dimensions of the convolution window.
+        strides: An integer or tuple/list of n integers,
+            specifying the strides of the convolution.
+            Specifying any stride value != 1 is incompatible with specifying
+            any `dilation_rate` value != 1.
+        padding: One of `"valid"` or `"same"` (case-insensitive).
+        data_format: A string,
+            one of `"channels_last"` or `"channels_first"`.
+            The ordering of the dimensions in the inputs.
+            `"channels_last"` corresponds to inputs with shape
+            `(batch, ..., channels)` while `"channels_first"` corresponds to
+            inputs with shape `(batch, channels, ...)`.
+            It defaults to the `image_data_format` value found in your
+            Keras config file at `~/.keras/keras.json`.
+            If you never set it, then it will be "channels_last".
+        dilation_rate: An integer or tuple/list of n integers, specifying
+            the dilation rate to use for dilated convolution.
+            Currently, specifying any `dilation_rate` value != 1 is
+            incompatible with specifying any `strides` value != 1.
+        activation: Activation function to use
+            (see [activations](../activations.md)).
+            If you don't specify anything, no activation is applied
+            (ie. "linear" activation: `a(x) = x`).
+        use_bias: Boolean, whether the layer uses a bias vector.
+        kernel_initializer: Initializer for the `kernel` weights matrix
+            (see [initializers](../initializers.md)).
+        bias_initializer: Initializer for the bias vector
+            (see [initializers](../initializers.md)).
+        kernel_regularizer: Regularizer function applied to
+            the `kernel` weights matrix
+            (see [regularizer](../regularizers.md)).
+        bias_regularizer: Regularizer function applied to the bias vector
+            (see [regularizer](../regularizers.md)).
+        activity_regularizer: Regularizer function applied to
+            the output of the layer (its "activation").
+            (see [regularizer](../regularizers.md)).
+        kernel_constraint: Constraint function applied to the kernel matrix
+            (see [constraints](../constraints.md)).
+        bias_constraint: Constraint function applied to the bias vector
+            (see [constraints](../constraints.md)).
+    """
+
+    def __init__(self,
+                 filters,
+                 kernel_size,
+                 connectivity_level=None,
+                 connectivity_decay=None,
+                 strides=1,
+                 padding='valid',
+                 data_format=None,
+                 dilation_rate=1,
+                 activation=None,
+                 use_bias=True,
+                 kernel_initializer='glorot_uniform',
+                 bias_initializer='zeros',
+                 kernel_regularizer=None,
+                 bias_regularizer=None,
+                 activity_regularizer=None,
+                 kernel_constraint=None,
+                 bias_constraint=None,
+                 **kwargs):
+        super(SparseConv2D, self).__init__(rank=2,
+                                           filters=filters,
+                                           kernel_size=kernel_size,
+                                           connectivity_level=connectivity_level,
+                                           connectivity_decay=connectivity_decay,
+                                           strides=strides,
+                                           padding=padding,
+                                           data_format=data_format,
+                                           dilation_rate=dilation_rate,
+                                           activation=activation,
+                                           use_bias=use_bias,
+                                           kernel_initializer=kernel_initializer,
+                                           bias_initializer=bias_initializer,
+                                           kernel_regularizer=kernel_regularizer,
+                                           bias_regularizer=bias_regularizer,
+                                           activity_regularizer=activity_regularizer,
+                                           kernel_constraint=kernel_constraint,
+                                           bias_constraint=bias_constraint,
+                                           **kwargs)
+
+    def get_config(self):
+        config = super(SparseConv2D, self).get_config()
+        config.pop('rank')
+        return config
+
+
+class SparseDepthwiseConv2D(SparseConv2D):
+    """Depthwise separable 2D convolution.
+
+    Depthwise Separable convolutions consists in performing
+    just the first step in a depthwise spatial convolution
+    (which acts on each input channel separately).
+    The `depth_multiplier` argument controls how many
+    output channels are generated per input channel in the depthwise step.
+
+    # Arguments
+        kernel_size: An integer or tuple/list of 2 integers, specifying the
+            height and width of the 2D convolution window.
+            Can be a single integer to specify the same value for
+            all spatial dimensions.
+        strides: An integer or tuple/list of 2 integers,
+            specifying the strides of the convolution
+            along the height and width.
+            Can be a single integer to specify the same value for
+            all spatial dimensions.
+            Specifying any stride value != 1 is incompatible with specifying
+            any `dilation_rate` value != 1.
+        padding: one of `"valid"` or `"same"` (case-insensitive).
+        depth_multiplier: The number of depthwise convolution output channels
+            for each input channel.
+            The total number of depthwise convolution output
+            channels will be equal to `filters_in * depth_multiplier`.
+        data_format: A string,
+            one of `"channels_last"` or `"channels_first"`.
+            The ordering of the dimensions in the inputs.
+            `"channels_last"` corresponds to inputs with shape
+            `(batch, height, width, channels)` while `"channels_first"`
+            corresponds to inputs with shape
+            `(batch, channels, height, width)`.
+            It defaults to the `image_data_format` value found in your
+            Keras config file at `~/.keras/keras.json`.
+            If you never set it, then it will be 'channels_last'.
+        activation: Activation function to use
+            (see [activations](../activations.md)).
+            If you don't specify anything, no activation is applied
+            (ie. 'linear' activation: `a(x) = x`).
+        use_bias: Boolean, whether the layer uses a bias vector.
+        depthwise_initializer: Initializer for the depthwise kernel matrix
+            (see [initializers](../initializers.md)).
+        bias_initializer: Initializer for the bias vector
+            (see [initializers](../initializers.md)).
+        depthwise_regularizer: Regularizer function applied to
+            the depthwise kernel matrix
+            (see [regularizer](../regularizers.md)).
+        bias_regularizer: Regularizer function applied to the bias vector
+            (see [regularizer](../regularizers.md)).
+        activity_regularizer: Regularizer function applied to
+            the output of the layer (its 'activation').
+            (see [regularizer](../regularizers.md)).
+        depthwise_constraint: Constraint function applied to
+            the depthwise kernel matrix
+            (see [constraints](../constraints.md)).
+        bias_constraint: Constraint function applied to the bias vector
+            (see [constraints](../constraints.md)).
+
+    # Input shape
+        4D tensor with shape:
+        `(batch, channels, rows, cols)`
+        if `data_format` is `"channels_first"`
+        or 4D tensor with shape:
+        `(batch, rows, cols, channels)`
+        if `data_format` is `"channels_last"`.
+
+    # Output shape
+        4D tensor with shape:
+        `(batch, filters, new_rows, new_cols)`
+        if `data_format` is `"channels_first"`
+        or 4D tensor with shape:
+        `(batch, new_rows, new_cols, filters)`
+        if `data_format` is `"channels_last"`.
+        `rows` and `cols` values might have changed due to padding.
+    """
+
+    def __init__(self,
+                 kernel_size,
+                 strides=(1, 1),
+                 padding='valid',
+                 depth_multiplier=1,
+                 data_format=None,
+                 activation=None,
+                 use_bias=True,
+                 depthwise_initializer='glorot_uniform',
+                 bias_initializer='zeros',
+                 depthwise_regularizer=None,
+                 bias_regularizer=None,
+                 activity_regularizer=None,
+                 depthwise_constraint=None,
+                 bias_constraint=None,
+                 **kwargs):
+        super(SparseDepthwiseConv2D, self).__init__(
+            filters=None,
+            kernel_size=kernel_size,
+            strides=strides,
+            padding=padding,
+            data_format=data_format,
+            activation=activation,
+            use_bias=use_bias,
+            bias_regularizer=bias_regularizer,
+            activity_regularizer=activity_regularizer,
+            bias_constraint=bias_constraint,
+            **kwargs)
+        self.depth_multiplier = depth_multiplier
+        self.depthwise_initializer = initializers.get(depthwise_initializer)
+        self.depthwise_regularizer = regularizers.get(depthwise_regularizer)
+        self.depthwise_constraint = constraints.get(depthwise_constraint)
+        self.bias_initializer = initializers.get(bias_initializer)
+
+    def build(self, input_shape):
+        if len(input_shape) < 4:
+            raise ValueError('Inputs to `DepthwiseConv2D` should have rank 4. '
+                             'Received input shape:', str(input_shape))
+        if self.data_format == 'channels_first':
+            channel_axis = 1
+        else:
+            channel_axis = 3
+        if input_shape[channel_axis] is None:
+            raise ValueError('The channel dimension of the inputs to '
+                             '`DepthwiseConv2D` '
+                             'should be defined. Found `None`.')
+        input_dim = int(input_shape[channel_axis])
+        depthwise_kernel_shape = (self.kernel_size[0],
+                                  self.kernel_size[1],
+                                  input_dim,
+                                  self.depth_multiplier)
+
+        self.depthwise_kernel = self.add_weight(
+            shape=depthwise_kernel_shape,
+            initializer=self.depthwise_initializer,
+            name='depthwise_kernel',
+            regularizer=self.depthwise_regularizer,
+            constraint=self.depthwise_constraint)
+
+        if self.use_bias:
+            self.bias = self.add_weight(shape=(input_dim * self.depth_multiplier,),
+                                        initializer=self.bias_initializer,
+                                        name='bias',
+                                        regularizer=self.bias_regularizer,
+                                        constraint=self.bias_constraint)
+        else:
+            self.bias = None
+        # Set input spec.
+        self.input_spec = InputSpec(ndim=4, axes={channel_axis: input_dim})
+        self.built = True
+
+    def call(self, inputs, training=None):
+        outputs = K.depthwise_conv2d(
+            inputs,
+            self.depthwise_kernel,
+            strides=self.strides,
+            padding=self.padding,
+            dilation_rate=self.dilation_rate,
+            data_format=self.data_format)
+
+        if self.use_bias:
+            outputs = K.bias_add(
+                outputs,
+                self.bias,
+                data_format=self.data_format)
+
+        if self.activation is not None:
+            return self.activation(outputs)
+
+        return outputs
+
+    def compute_output_shape(self, input_shape):
+        if self.data_format == 'channels_first':
+            rows = input_shape[2]
+            cols = input_shape[3]
+            out_filters = input_shape[1] * self.depth_multiplier
+        elif self.data_format == 'channels_last':
+            rows = input_shape[1]
+            cols = input_shape[2]
+            out_filters = input_shape[3] * self.depth_multiplier
+
+        rows = conv_utils.conv_output_length(rows, self.kernel_size[0],
+                                             self.padding,
+                                             self.strides[0])
+        cols = conv_utils.conv_output_length(cols, self.kernel_size[1],
+                                             self.padding,
+                                             self.strides[1])
+        if self.data_format == 'channels_first':
+            return (input_shape[0], out_filters, rows, cols)
+        elif self.data_format == 'channels_last':
+            return (input_shape[0], rows, cols, out_filters)
+
+    def get_config(self):
+        config = super(DepthwiseConv2D, self).get_config()
+        config.pop('filters')
+        config.pop('kernel_initializer')
+        config.pop('kernel_regularizer')
+        config.pop('kernel_constraint')
+        config['depth_multiplier'] = self.depth_multiplier
+        config['depthwise_initializer'] = (
+            initializers.serialize(self.depthwise_initializer))
+        config['depthwise_regularizer'] = (
+            regularizers.serialize(self.depthwise_regularizer))
+        config['depthwise_constraint'] = (
+            constraints.serialize(self.depthwise_constraint))
+        return config
